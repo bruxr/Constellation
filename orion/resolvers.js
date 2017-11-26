621adf57a59259e1f5a0c6372a88f038866812b3
Brux Romuar <brux.romuar@gmail.com>
2017-11-26 15:16:45 +0800

    Update resolvers to use new db APIs.

--- orion/resolvers.js
+++ orion/resolvers.js
@@ -1,12 +1,17 @@
+const escape = require('validator/lib/escape');
+
 const db = require('./database');
 
 module.exports = {
   Query: {
-    wallets: () => db.wallets(),
+    wallets: () => db.getAll('wallets'),
   },
   Mutation: {
     createWallet(_, { name, balance }) {
-      return db.wallets({ name, balance });
+      return db.put('wallets', {
+        name: escape(name),
+        balance,
+      });
     },
   },
 };
